662a1893bb07036ab02a636f5522a1da1f493d79
Nicolas <gg491908@ens.uqam.ca>
2021-11-07 16:49:33 -0500

    Conception de la méthode checkHeureTotal et initialisation de la classe Géologue

--- projet-equipe14/src/main/java/Géologue.java
+++ projet-equipe14/src/main/java/Géologue.java
@@ -1,2 +1,32 @@
+import net.sf.json.JSONArray;
+import net.sf.json.JSONObject;
+
 public class Géologue {
+
+    public boolean isGeologue;
+
+    public Géologue() {
+        this.isGeologue = true;
+    }
+
+    public boolean isGeologue() {
+        return isGeologue;
+    }
+
+    public void setGeologue(boolean geologue) {
+        isGeologue = geologue;
+    }
+
+    public boolean checkHeureTotal(JSONArray activities){
+        int heures = 0;
+
+        for (Object o : activities) {
+            JSONObject activity = (JSONObject) o;
+            heures += Integer.parseInt(activity.get("heures").toString());
+        }
+
+        return heures >= 55;
+    }
+
+
 }
